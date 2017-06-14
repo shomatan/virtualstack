6b1340de30209c621f7e4a8bdbda933b5d742785
Shoma Nishitateno <shoma416@gmail.com>
2017-06-14 13:42:33 +0900

    Add vue

---
+++ vue/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="heading">
+            <h1>Repositories</h1>
+        </div>
+        <h2></h2>
+        <div>
+            <div class="panel panel-default">
+                <div class="panel-heading">Images</div>
+            </div>
+        </div>
+    </div>
+</template>
