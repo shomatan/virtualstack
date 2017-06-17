4a3988e4cb8361693f7c1390ae3eb6bb44ed6235
Shoma Nishitateno <shoma416@gmail.com>
2017-06-17 20:30:15 +0900

    Remove circe dependency

--- app/controllers/api/v1/GitRepositoryController.scala
+++ app/controllers/api/v1/GitRepositoryController.scala
@@ -3,18 +3,21 @@ package com.github.virtualstack.controllers.api.v1
 import javax.inject.{Inject, Singleton}
 
 import com.github.virtualstack.gitbucket.client.GitbucketClient
+import com.github.virtualstack.gitbucket.client.Repository
+
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json.Json
 import scala.concurrent.ExecutionContext.Implicits.global
-import io.circe.syntax._
-import io.circe.generic.auto._
 
 @Singleton
 class GitRepositoryController @Inject()(client: GitbucketClient) extends Controller {
 
+  implicit val repositoryWrites = Json.writes[Repository]
+
   def all() = Action.async { implicit request =>
 
     client.getRepositories().map { aa =>
-      Ok(aa.asJson.noSpaces)
+      Ok(Json.toJson(aa))
     }
   }
 }
