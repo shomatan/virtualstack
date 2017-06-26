7e6f1bdade4b75f7ba0f4d50e571c674205af463
Shoma Nishitateno <shoma416@gmail.com>
2017-06-26 16:24:39 +0900

    Fix label

--- vue/components/auth/Login.vue
+++ vue/components/auth/Login.vue
@@ -8,7 +8,7 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <div class="panel panel-default">
-                            <div class="panel-heading">Sign Up</div>
+                            <div class="panel-heading">Login</div>
                             <div class="panel-body">
 
                                 <div class="form-group">
