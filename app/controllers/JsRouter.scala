942fcac11287c96937fdde23cd9c6171f449a797
Shoma Nishitateno <shoma416@gmail.com>
2017-06-29 11:36:06 +0900

    Refactor HomeController

--- app/controllers/JsRouter.scala
+++ app/controllers/JsRouter.scala
@@ -6,10 +6,14 @@ import play.api.routing._
 class JsRouter extends Controller {
 
   def javascriptRoutes = Action { implicit request =>
+    import routes.javascript._
     import api.v1.routes.javascript._
     import api.v1.auth.routes.javascript._
     Ok(
       JavaScriptReverseRouter("jsRoutes")(
+        HomeController.index,
+        HomeController.signOut,
+        HomeController.profile,
         ImageRepositoryController.all,
         ImageRepositoryController.detail,
         GitRepositoryController.all,
